85701db743f986837ca0a49da6f71746cd251d9b
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2023-01-01 20:17:00 +0530

    Add files via upload

--- D_Coprime.cpp
+++ D_Coprime.cpp
@@ -1,46 +1,82 @@
-#include "bits/stdc++.h"
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
- 
-#define ll long long
- 
-#define       forn(i,n)              for(int i=0;i<n;i++)
-#define          all(v)              v.begin(), v.end()
-#define         rall(v)              v.rbegin(),v.rend()
- 
-#define            pb                push_back
-#define          sz(a)               (int)a.size()
-
-vector<int> pairs[1001];
-void solve() {
-    int n; cin >> n;
-    vector<int> id[1001];
-    for(int i = 1; i <= n; ++i) {
-        int x; cin >> x;
-        id[x].push_back(i);
-    }
-    int ans = -1;
-    for(int i = 1; i <= 1000; ++i) {
-        for(int j: pairs[i]) {
-            if(!id[i].empty() && !id[j].empty()) {
-                ans = max(ans, id[i].back() + id[j].back());
-            }
-        }
-    }
-    cout << ans << "\n";
-}   
- 
-int32_t main() {
-    for(int i = 1; i <= 1000; ++i) {
-        for(int j = 1; j <= 1000; ++j) {
-            if(__gcd(i, j) == 1) {
-                pairs[i].push_back(j);
-            }
-        }
-    }
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int t = 1;
-    cin >> t;
-    while(t--) {
-        solve();
-    }
+                    
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long> vl;
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+typedef pair<long,long> vll;
+typedef pair<int,int> pi;
+                               
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Sieve of Eratosthenes:
+vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                               
+#define pb(x) push_back(x)
+#define mp make_pair
+#define Max(x,y,z) max(x,max(y,z))
+#define Min(x,y,z) min(x,min(y,z))
+#define fori(i,a,n) for(int i=a;i<n;i++)
+#define forj(j,b,n) for(int j=b;j<n;j++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define pi (3.141592653589)
+#define MOD 1000000007
+                      
+ll factorial(ll n){
+ll ans=1;
+for(int i=1; i<=n; i++)
+ans = (ans*i) % MOD;
+return ans;
+}
+                            
+bool isPrime(ll n){
+if (n <= 1)
+return false;
+if (n <= 3)
+return true;
+if (n % 2 == 0 || n % 3 == 0)
+return false;
+for (ll i = 5; i * i <= n; i = i + 6){
+if (n % i == 0 || n % (i + 2) == 0)
+return false;
+}
+return true;
+}
+                    
+//ITS ABOUT DRIVE, ITS ABOUT POWER!!
+//WE STAY HUNGRY, WE DEVOUR!!
+//AIM -> WORK -> ACHIEVE -> REPEAT Carpe Diem!!
+                    
+void solve()
+{
+   int n;int x;cin>>n;
+   vector<int> v(1001,-1);
+   for(int i=1;i<=n;i++){
+       cin>>x;
+       v[x]=i;
+   }
+   int ans=-1;
+   for(int i=1;i<1001;i++){
+       if(v[i]!=-1){
+           for(int j=1;j<1001;j++){
+               if(v[j]!=-1 && __gcd(i,j)==1){
+                    ans=max(ans,v[i]+v[j]);
+               }
+           }
+       }
+   }
+   cout<<ans<<endl;
+}
+                    
+signed main()
+{
+int t=1;
+cin>>t;
+while(t--)
+{
+   solve();
+}
 }
