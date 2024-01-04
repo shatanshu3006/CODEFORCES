dba78b733709b9b23f81ea528213a8f96efb1729
Your Name <shatanshubodkhe@gmail.com>
2024-01-04 20:41:28 +0530

    recent

---
+++ B_Erase_First_or_Second_Letter.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+                    
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long> vl;
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+typedef pair<long,long> vll;
+typedef pair<int,int> pi;
+                               
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Sieve of Eratosthenes:
+vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                               
+#define pb(x) push_back(x)
+#define mp make_pair
+#define Max(x,y,z) max(x,max(y,z))
+#define Min(x,y,z) min(x,min(y,z))
+#define fori(i,a,n) for(int i=a;i<n;i++)
+#define forj(j,b,n) for(int j=b;j<n;j++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define pi (3.141592653589)
+#define MOD 1000000007
+                      
+ll factorial(ll n){
+ll ans=1;
+for(int i=1; i<=n; i++)
+ans = (ans*i) % MOD;
+return ans;
+}
+                            
+bool isPrime(ll n){
+if (n <= 1)
+return false;
+if (n <= 3)
+return true;
+if (n % 2 == 0 || n % 3 == 0)
+return false;
+for (ll i = 5; i * i <= n; i = i + 6){
+if (n % i == 0 || n % (i + 2) == 0)
+return false;
+}
+return true;
+}
+                    
+//ITS ABOUT DRIVE, ITS ABOUT POWER!!
+//WE STAY HUNGRY, WE DEVOUR!!
+//AIM -> WORK -> ACHIEVE -> REPEAT Carpe Diem!!
+
+void func(int ind,string s,set<string>&ans){
+    if(ind==s.length())return;
+    string str1="";
+    string str2="";
+    str2+=s[ind];
+    for(int i=ind+1;i<s.length();i++){
+        str1+=s[i];
+    }
+    for(int i=ind+2;i<s.length();i++){
+        str2+=s[i];
+    }
+    if(str1=="")return;
+    if(str2=="")return;
+    ans.insert(str1);
+    ans.insert(str2);
+    func(ind+1,s,ans);
+    
+}
+void solve()
+{
+   int n;cin>>n;
+   string s;cin>>s;
+   ll ans=0;
+   set<char>st;
+   for(int i=0;i<n;i++){
+       st.insert(s[i]);
+       ans+=st.size();
+   }
+   cout<<ans<<endl;
+}
+                    
+signed main()
+{
+int t=1;
+cin>>t;
+while(t--)
+{
+   solve();
+}
+}
