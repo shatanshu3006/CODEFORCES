f08de71f1d54328faf7761fa034cbca4505e575e
Your Name <shatanshubodkhe@gmail.com>
2024-07-21 01:16:53 +0530

    new

--- C_Hungry_Games.cpp
+++ C_Hungry_Games.cpp
@@ -2,98 +2,28 @@
 #include <bits/stdc++.h>
 using namespace std;
                     
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<long> vl;
-#define all(x) begin(x), end(x)
-#define rall(x) x.rbegin(), x.rend()
-typedef pair<long,long> vll;
-typedef pair<int,int> pi;
-                               
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//Sieve of Eratosthenes:
-vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                               
-#define pb(x) push_back(x)
-#define mp make_pair
-#define Max(x,y,z) max(x,max(y,z))
-#define Min(x,y,z) min(x,min(y,z))
-#define fori(i,a,n) for(int i=a;i<n;i++)
-#define forj(j,b,n) for(int j=b;j<n;j++)
-#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-#define pi (3.141592653589)
-#define MOD 1000000007
-                      
-ll factorial(ll n){
-ll ans=1;
-for(int i=1; i<=n; i++)
-ans = (ans*i) % MOD;
-return ans;
-}
-                            
-bool isPrime(ll n){
-if (n <= 1)
-return false;
-if (n <= 3)
-return true;
-if (n % 2 == 0 || n % 3 == 0)
-return false;
-for (ll i = 5; i * i <= n; i = i + 6){
-if (n % i == 0 || n % (i + 2) == 0)
-return false;
-}
-return true;
-}
-                    
-//ITS ABOUT DRIVE, ITS ABOUT POWER!!
-//WE STAY HUNGRY, WE DEVOUR!!
-//AIM -> WORK -> ACHIEVE -> REPEAT Carpe Diem!!
+#define int long long
                     
 void solve()
 {
-   ll n,x;
-   cin>>n>>x;
-   vector<ll>a(n);
-   for(ll i=0;i<n;i++){
-       cin>>a[i];
-   }
-   ll i=0,j=0,sum=0,ans=0,ans2=0;
-        while(j<n)
-        {
-            sum += a[j];
-            while(sum*(j-i+1) >= x) sum -= a[i++];
-            ans += j-i+1;
-            j++;
+   int n,x;cin>>n>>x;
+    vector<int> arr(n);for(int i =0;i<n;i++)cin>>arr[i];
+    vector<int> prefix(n,0); prefix[0] = arr[0];
+    for(int i = 1;i<n;i++)prefix[i] = prefix[i-1]+arr[i];
+    vector<int> dp(n,0);
+    for(int i = n-1;i>=0;i--){
+        int prevSum = (i-1 >=0 ? prefix[i-1] : 0);
+        int j = upper_bound(prefix.begin(),prefix.end(),prevSum+x) - prefix.begin();
+        if(j < n){
+            dp[i] = 1 + ((j+1 < n) ? dp[j+1] : 0);
         }
-        //ans is the number of subs less than x
-
-        map<ll,ll>mp;
-        mp[0]=1;
-         j=0; sum=0;ll cnt=0;
-        while(j<n){
-            sum+=a[j];
-            ll rem=sum-x;
-
-            //already in the map 
-            if(mp.find(rem)!=mp.end()){
-                cnt+=mp[rem];
-            }
-            //if sum is not in map then add it to mp
-            if(mp.find(sum)==mp.end()){
-                mp[sum]=1;
-            }
-            else{
-            mp[sum]++;
-            }
-            j++;
-        }
-
-        cout<<ans+cnt<<endl;
-        
-        
+    }
+    int ans = 0;
+    for(int i =0;i<n;i++){
+        ans += (n-i)-dp[i];
+    }
+    cout<<ans<<endl;
    
-
 }
                     
 signed main()
